abb19761f805a4eb05404cd690b2c21a254362ed
Peng Qin <peng.qin@plexure.com>
2025-09-11 06:09:05 +1200

    bump the version to include bug fixes

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     description="Integrate Oanda-V20 API into backtrader",
     long_description=long_description,
     license='GNU General Public License Version 3',
-    url="https://github.com/happydasch/btoandav20",
+    url="https://github.com/ftkghost/btoandav20",
     packages=setuptools.find_packages(),
     install_requires=[
         'backtrader>=1.9',
